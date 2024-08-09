0361b50f262522a9a57c227be485652ca7b2a74f
moralesdaxz <alfredojose.92@gmail.com>
2024-08-09 17:05:35 +0200

    ✅ Manejando la fecha y hora desde Front

--- src/utils/invoice.ts
+++ src/utils/invoice.ts
@@ -1,28 +1,35 @@
-export const fetchMadridDateTime = async (): Promise<{ time: string, date: string }> => {
-  const URL_API = process.env.NEXT_PUBLIC_URL_API_TIME || 'https://worldtimeapi.org/api/timezone/Europe/Madrid';
-  
+export const fetchMadridDateTime = async (): Promise<{
+  time: string;
+  date: string;
+}> => {
+  const URL_API =
+    process.env.NEXT_PUBLIC_URL_API_TIME ||
+    "https://worldtimeapi.org/api/timezone/Europe/Madrid";
+
   try {
     const response = await fetch(URL_API);
     if (!response.ok) {
-      throw new Error('Network response was not ok');
+      throw new Error("Network response was not ok");
     }
     const data = await response.json();
     const date = new Date(data.datetime);
-
+    const hours = String(date.getHours()).padStart(2, "0");
+    const minutes = String(date.getMinutes()).padStart(2, "0");
     // Formatear la hora
-    const time = `${String(date.getHours()).padStart(2, '0')}:${String(date.getMinutes()).padStart(2, '0')}`;
+    const time = `${hours + minutes}`;
+    // Formatear la hora
 
     // Formatear la fecha
-    const formattedDate = `${date.getFullYear()}${String(date.getMonth() + 1).padStart(2, '0')}${String(date.getDate()).padStart(2, '0')}`;
+    const formattedDate = `${date.getFullYear()}${String(
+      date.getMonth() + 1
+    ).padStart(2, "0")}${String(date.getDate()).padStart(2, "0")}`;
 
-    return { time, date: formattedDate };
+    return { time: time, date: formattedDate };
   } catch (error) {
-    console.error('Error fetching date and time:', error);
-    throw new Error('Could not fetch the date and time from the API');
+    console.error("Error fetching date and time:", error);
+    throw new Error("Could not fetch the date and time from the API");
   }
 };
-
-
 
 /* // Ejemplo de uso para la hora en formato 24 horas
 export const displayCurrentTime = async () => {
@@ -32,4 +39,3 @@ export const displayCurrentTime = async () => {
 
 
 displayCurrentTime(); */
-
