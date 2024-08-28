eb9d1ca44d4aace61dd040856887fd4a9e0092d2
Morales <125131236+MoralesDaxz@users.noreply.github.com>
2024-08-28 19:24:09 +0200

    🐛 fetch urlCoffee! + "products"

--- src/context/GetProducts.tsx
+++ src/context/GetProducts.tsx
@@ -38,7 +38,7 @@ export const GetProductsProvider: FC<{ children: React.ReactNode }> = ({
   useEffect(() => {
     const getProduct = async () => {
       try {
-        const promise = await fetch(urlCoffee! + "/products");
+        const promise = await fetch(urlCoffee! + "products");
         const response = await promise.json();
         return setCoffee(response.products);
       } catch (error) {
@@ -46,9 +46,9 @@ export const GetProductsProvider: FC<{ children: React.ReactNode }> = ({
         return console.log(error);
       }
     };
-    return () => {
-      getProduct()
-    };
+
+    getProduct();
+    return;
   });
   return (
     <GetProducts.Provider
